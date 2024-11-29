75790579cefb45ea2b2756c7e10a8fb0870f5c8c
rogersds <rogger_souza@hotmail.com>
2024-11-29 17:01:49 -0300

    JPA repository, dependency injection, database seeding all

---
+++ src/main/java/com/educandoweb/course/testeCoomit.java
@@ -0,0 +1,16 @@
+package com.educandoweb.course;
+
+public class testeCoomit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 12;
+		int b = 100;
+		
+		int mult = a * b;
+
+	}
+
+}

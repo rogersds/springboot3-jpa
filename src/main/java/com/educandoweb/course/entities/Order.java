8ccbf04cf9c08132c0738e1c053933c02faaf7bc
rogersds <rogger_souza@hotmail.com>
2024-12-03 16:08:10 -0300

    OrderStatus Enums

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
@@ -27,7 +28,11 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
+	
 
+	private Integer orderStatus;
+	
 	// criando associações e declarando que essa classe é estrangeira(muitos para
 	// um)
 	@ManyToOne
@@ -37,10 +42,13 @@ public class Order implements Serializable {
 	public Order() {
 
 	}
+	
+	
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -58,6 +66,20 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus) ;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		
+		if(orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		
+		}
 	}
 
 	public User getClient() {
